57344521032a058c51b4985ebc38c09f7d683d46
McHenry <lukemchenry311@gmail.com>
2021-09-30 23:49:29 -0600

    navlinks working

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function NavMenu() {
-  return <h1> Hello from the NavMenu</h1>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/projects">Projects</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
