f0393083da1f2c7eba61e662908505278b822f2b
McHenry <lukemchenry311@gmail.com>
2021-10-01 00:17:36 -0600

    pages, styles, woes

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavMenuStyles = styled.div`
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%auto;
+  padding: 1rem 0;
+  background: var(--dark-bg);
+  ul {
+    max-width: 1200px;
+    margin: 0 auto;
+    width: 90%;
+    text-align: center;
+    li {
+      display: inline-block;
+      border-radius: 8px;
+      transition: 0.3s ease background-color;
+      &:hover {
+        background-color: var(--deep-black);
+      }
+    }
+    a {
+      display: inline-block;
+      font-family: 'RobotoMono Regular';
+    }
+  }
+`;
 
 export default function NavMenu() {
   return (
-    <div>
+    <NavMenuStyles>
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
@@ -18,6 +47,6 @@ export default function NavMenu() {
           <NavLink to="/contact">Contact</NavLink>
         </li>
       </ul>
-    </div>
+    </NavMenuStyles>
   );
 }
