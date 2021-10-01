0d995c0ef55756e83ab29613a9c11663893a1705
McHenry <lukemchenry311@gmail.com>
2021-09-30 23:40:21 -0600

    beginning navmenu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import NavMenu from './components/NavMenu';
 
 export default function App() {
   return (
     <>
-      <h1>Welcome to the portfolio project</h1>
-      <h3>-Luke McHenry</h3>
+      <NavMenu />
     </>
   );
 }
