5d4a43e97d1fd63f362f53e59b763946679537ab
McHenry <lukemchenry311@gmail.com>
2021-10-02 05:44:13 -0600

    projects data

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,45 +1,46 @@
 import { v4 as uuidv4 } from 'uuid';
-import ProjectImg from '../images/projectImg.png';
-import UTrackerImg from '../images/utracker.jpg';
-import GreenCtgImg from '../images/greenctg.jpg';
-import CoinTrackerImg from '../images/cointracker.jpg';
-import CavinImg from '../images/cavinimg.jpg';
+
+import ProjOne from '../images/projectone.png';
+import ProjTwo from '../images/projecttwo.png';
+import ProjThree from '../images/projectthree.png';
+import ProjFour from '../images/projectfour.png';
+import ProjFive from '../images/projectfive.png';
 
 const projects = [
   {
     id: uuidv4(),
-    name: 'U Tracker',
+    name: 'Coinvrt',
     desc:
-      'An application to track your all data from one place. I developed the website and the mobile app',
-    img: UTrackerImg,
+      'An application to track cryptocurrency data. I developed the website with my partners, Robert and Jordan. Repository for project: https://github.com/LukeMcHenry311/weekly-group-project URL of deployed application: https://lukemchenry311.github.io/weekly-group-project/',
+    img: ProjOne,
   },
   {
     id: uuidv4(),
-    name: 'Green CTG',
+    name: 'Hogwarts Library',
     desc:
-      'An app to help people to get an overview of how they can make the city beautiful.',
-    img: GreenCtgImg,
+      'A fun app to find your harry potter house, or create wizards and spells with other users. Repository for project: https://github.com/Jsieler/wizard-sorting-quiz URL of deployed application: https://pacific-scrubland-02824.herokuapp.com/',
+    img: ProjTwo,
   },
   {
     id: uuidv4(),
-    name: 'Coin Tracker',
+    name: 'Team-Up',
     desc:
-      'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
-    img: CoinTrackerImg,
+      'Team-Up is a gaming social networking app, where you can follow your favorite games. As well as add users with similar gaming interests to team up! Repository for project: https://github.com/Jsieler/team-up URL of deployed application: https://github.com/Jsieler/team-up',
+    img: ProjThree,
   },
   {
     id: uuidv4(),
-    name: "Cavin's Portfolio",
+    name: 'Weather Dashboard',
     desc:
-      'A portfolio for Cavin jr. A artist from New york city. The portfolio is made using ReactJs and GatsbyJs.',
-    img: CavinImg,
+      'A simple website to tell the 5-day weather forecast for any city in the world. Repository for assignment: https://github.com/LukeMcHenry311/Server-Side-Weather URL of deployed application https://lukemchenry311.github.io/Server-Side-Weather/',
+    img: ProjFour,
   },
   {
     id: uuidv4(),
-    name: 'Tracking Soft',
+    name: 'Team-Profile-Generator-Xioma',
     desc:
-      'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
-    img: ProjectImg,
+      'A back-end node based application to generate a team-profile dynamically with minimal input from the user. Repository for assignment: https://github.com/LukeMcHenry311/Team-Profile-Generator-Xioma URL of deployed application: There is no deployed application for this assignment since it is back-end, to look over the assignment itself follow the instructions on the github link to install.',
+    img: ProjFive,
   },
 ];
 
