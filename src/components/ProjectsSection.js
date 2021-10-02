e567976fb5270ac3ccd56c910b67ebe1d80115f6
McHenry <lukemchenry311@gmail.com>
2021-10-02 06:08:03 -0600

    working carousel

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,22 +1,48 @@
 import React from 'react';
-import { Swiper } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper';
+import styled from 'styled-components';
 import SectionTitle from './SectionTitle';
 import projects from '../assets/data/projects';
+import ProjectItem from './ProjectItem';
+import 'swiper/swiper-bundle.min.css';
+
+SwiperCore.use([Navigation]);
+
+const ProjectSectionStyle = styled.div`
+  padding: 10rem 0;
+  .projects__allItems {
+    display: flex;
+    gap: 3rem;
+    margin-top: 5rem;
+  }
+  .swiper-container {
+    padding-top: 8rem;
+    max-width: 100%;
+  }
+`;
 
 export default function ProjectsSection() {
   return (
-    <div>
+    <ProjectSectionStyle>
       <div className="container">
-        <SectionTitle heading="Projects" subheading="some of my recent work" />
+        <SectionTitle
+          heading="Projects"
+          subheading="swipe through some of my recent work"
+        />
         <div className="projects__allItems">
-          <Swiper>
+          <Swiper spaceBetween={30} slidesPerView={2} navigation>
             {projects.map((project, index) => {
               if (index >= 5) return;
-              return <div>This is our project</div>;
+              return (
+                <SwiperSlide key={project.id}>
+                  <ProjectItem />
+                </SwiperSlide>
+              );
             })}
           </Swiper>
         </div>
       </div>
-    </div>
+    </ProjectSectionStyle>
   );
 }
