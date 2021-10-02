cc071452f528405e4bb6edec700c75ea9d4908f5
McHenry <lukemchenry311@gmail.com>
2021-10-02 00:32:46 -0600

    fixed arrow problems, resized image

--- src/components/PText.js
+++ src/components/PText.js
@@ -5,7 +5,7 @@ const PStyle = styled.div`
   max-width: 500px;
   margin: 0 auto;
   font-size: 1.8 rem;
-  line-height: 1.3 rem;
+  line-height: 1.3 em;
   @media only screen and (max-width: 768px) {
     font-size: 1.4rem;
   }
